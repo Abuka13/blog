8613b2c2cd331cfc6c61203a010fab4d46fe2f3c
Abuka13 <abukaozbekbay@gmail.com>
2025-12-05 16:20:40 +0500

    Created the post form

--- django_app/urls.py
+++ django_app/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from django_app import views
 urlpatterns = [
     path("main/", views.main, name="main"),
+
+
+    ##TODO POSTS
     path("posts/", views.get_posts, name="posts"),
-    path("posts/<int:pk>/", views.post_detail, name="post_detail")
+    path("posts/<int:pk>/", views.post_detail, name="post_detail"),
+    path("posts/create/", views.post_form, name="post_form")
 ]
--- django_app/views/post.py
+++ django_app/views/post.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
 from django_app import models
 from django.core.paginator import Paginator
 
 
 def get_posts(request):
     if request.method == "GET":
-        posts = models.Post.objects.all().order_by('-created_at')
+        posts = models.Post.objects.all().order_by('created_at')
         selected_page = request.GET.get(key="page", default = 1)
         limit_post_by_page = 3
         paginator = Paginator(posts, limit_post_by_page)
@@ -16,4 +18,18 @@ def post_detail(request, pk):
     post = models.Post.objects.get(id=pk)
     return render(request, 'posts/post_detail.html', context={"post":post})
 
+
+def post_form(request):
+    if request.method == "GET":
+        return render(request, 'posts/post_form.html')
+    elif request.method == "POST":
+        author = request.user
+        title = str(request.POST["title"])
+        description = str(request.POST["description"])
+        models.Post.objects.create(
+            author = author,
+            title = title,
+            description = description
+        )
+        return redirect(reverse(get_posts))
 
